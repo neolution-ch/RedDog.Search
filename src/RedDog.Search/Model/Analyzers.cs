c91bc75d0b9c6d96d65a1d6aa10916a3dc9d7bf7
Sandro Ciervo <sandro.ciervo@neolution.ch>
2015-05-21 10:23:22 +0200

    Refactored the Analyzers class to introduce the new Microsoft Analyzers added in 2015-02-28-Preview

--- src/RedDog.Search/Model/Analyzers.cs
+++ src/RedDog.Search/Model/Analyzers.cs
@@ -2,34 +2,92 @@
 {
     public static class Analyzers
     {
-        public const string Arabic = "ar.lucene";
-        public const string Brazilian = "pt-Br.lucene";
-        public const string ChineseSimplified = "zh-Hans.lucene";
-        public const string ChineseTraditional = "zh-Hant.lucene";
-        public const string Czech = "cs.lucene";
-        public const string Danish = "da.lucene";
-        public const string Dutch = "nl.lucene";
-        public const string German = "de.lucene";
-        public const string Greek = "el.lucene";
-        public const string English = "en.lucene";
-        public const string Finnish = "fi.lucene";
-        public const string French = "fr.lucene";
-        public const string Hindi = "hi.lucene";
-        public const string Hungarian = "hu.lucene";
-        public const string IndonesianBahasa = "id.lucene";
-        public const string Italian = "it.lucene";
-        public const string Japanese = "ja.lucene";
-        public const string Korean = "ko.lucene";
-        public const string Latvian = "lv.lucene";
-        public const string Norwegian = "no.lucene";
-        public const string Polish = "pl.lucene";
-        public const string Portuguese = "pt-Pt.lucene";
-        public const string Romanian = "ro.lucene";
-        public const string Russian = "ru.lucene";
-        public const string Spanish = "es.lucene";
-        public const string Swedish = "sv.lucene";
-        public const string Turkish = "tr.lucene";
-        public const string Thai = "th.lucene";
-        public const string StandardAcsiiFolding = "standardasciifolding.lucene";
+        public static class Lucene
+        {
+            public const string Arabic = "ar.lucene";
+            public const string Brazilian = "pt-Br.lucene";
+            public const string ChineseSimplified = "zh-Hans.lucene";
+            public const string ChineseTraditional = "zh-Hant.lucene";
+            public const string Czech = "cs.lucene";
+            public const string Danish = "da.lucene";
+            public const string Dutch = "nl.lucene";
+            public const string English = "en.lucene";
+            public const string Finnish = "fi.lucene";
+            public const string French = "fr.lucene";
+            public const string German = "de.lucene";
+            public const string Greek = "el.lucene";
+            public const string Hindi = "hi.lucene";
+            public const string Hungarian = "hu.lucene";
+            public const string IndonesianBahasa = "id.lucene";
+            public const string Italian = "it.lucene";
+            public const string Japanese = "ja.lucene";
+            public const string Korean = "ko.lucene";
+            public const string Latvian = "lv.lucene";
+            public const string Norwegian = "no.lucene";
+            public const string Polish = "pl.lucene";
+            public const string PortugueseBrazil = "pt-Br.lucene";
+            public const string PortuguesePortugal = "pt-Pt.lucene";
+            public const string Romanian = "ro.lucene";
+            public const string Russian = "ru.lucene";
+            public const string Spanish = "es.lucene";
+            public const string Swedish = "sv.lucene";
+            public const string Turkish = "tr.lucene";
+            public const string Thai = "th.lucene";
+            public const string StandardAcsiiFolding = "standardasciifolding.lucene";
+        }
+
+        public static class Microsoft
+        {
+            public const string Arabic = "ar.microsoft";
+            public const string Bangla = "bg.microsoft";
+            public const string Bulgarian = "bn.microsoft";
+            public const string Catalan = "ca.microsoft";
+            public const string ChineseSimplified = "zh-Hans.microsoft";
+            public const string ChineseTraditional = "zh-Hant.microsoft";
+            public const string Croatian = "hr.microsoft";
+            public const string Czech = "cs.microsoft";
+            public const string Danish = "da.microsoft";
+            public const string Dutch = "nl.microsoft";
+            public const string English = "en.microsoft";
+            public const string Estonian = "et.microsoft";
+            public const string Finnish = "fi.microsoft";
+            public const string French = "fr.microsoft";
+            public const string German = "de.microsoft";
+            public const string Greek = "el.microsoft";
+            public const string Gujarati = "gu.microsoft";
+            public const string Hebrew = "he.microsoft";
+            public const string Hindi = "hi.microsoft";
+            public const string Hungarian = "hu.microsoft";
+            public const string Icelandic = "is.microsoft";
+            public const string IndonesianBahasa = "id.lucene";
+            public const string Italian = "it.microsoft";
+            public const string Japanese = "ja.microsoft";
+            public const string Kannada = "ka.microsoft";
+            public const string Latvian = "lv.microsoft";
+            public const string Lithuanian = "lt.microsoft";
+            public const string Malayalam = "ml.microsoft";
+            public const string MalayLatin = "ms.microsoft";
+            public const string Marathi = "mr.microsoft";
+            public const string Norwegian = "no.microsoft";
+            public const string Polish = "pl.microsoft";
+            public const string PortugueseBrazil = "pt-Br.microsoft";
+            public const string PortuguesePortugal = "pt-Pt.microsoft";
+            public const string Punjabi = "pa.microsoft";
+            public const string Romanian = "ro.microsoft";
+            public const string Russian = "ru.microsoft";
+            public const string SerbianCyrillic = "sr-cyrillic.microsoft";
+            public const string SerbianLatin = "sr-latin.microsoft";
+            public const string Slovak = "sk.microsoft";
+            public const string Slovenian = "sl.microsoft";
+            public const string Spanish = "es.microsoft";
+            public const string Swedish = "sv.microsoft";
+            public const string Tamil = "ta.microsoft";
+            public const string Telugu = "te.microsoft";
+            public const string Thai = "th.microsoft";
+            public const string Turkish = "tr.microsoft";
+            public const string Ukrainian = "uk.microsoft";
+            public const string Urdu = "ur.microsoft";
+            public const string Vietnamese = "vi.microsoft";
+        }
     }
 }
